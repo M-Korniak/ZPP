aaff0619051cd52f9422bf88dd8bc84d89bc968b
Zuz <zk439532@students.mimuw.edu.pl>
2025-03-08 23:41:33 +0100

    Imported the Transformer definition from the model module for easier use in the future; changed the Trainer name to avoid misunderstanding.

--- generator/tests/model/test_model.py
+++ generator/tests/model/test_model.py
@@ -19,6 +19,8 @@ import torch
 import torch.nn.functional as F
 from torch import nn
 
+import src.model.model as model
+
 # Constants definiton
 if torch.cuda.is_available():
     DEVICE = torch.device("cuda")
@@ -55,224 +57,7 @@ class ModelArgs:
 
 
 # Modified inner transformer model defintion
-class RMSNorm(torch.nn.Module):
-    """
-    Root Mean Square Layer Normalization (RMSNorm)
-
-    Args:
-    - dim (int): The input dimension
-    - eps (float): A small value to prevent division by zero
-
-    Attributes:
-    - eps (float): A small value to prevent division by zero
-    - weight (torch.nn.Parameter): The learnable weight parameter
-    """
-    def __init__(self, dim: int, eps: float = 1e-6):
-        super().__init__()
-        self.eps = eps
-        self.weight = nn.Parameter(torch.ones(dim))
-
-    def _norm(self, x):
-        return x * torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps)
-
-    def forward(self, x):
-        output = self._norm(x.float()).type_as(x)
-        return output * self.weight
-
-
-def precompute_freqs_cis(dim: int, end: int, theta: float = 10000.0):
-    """
-    Precompute the frequencies for the rotary embeddings.
-
-    Args:
-    - dim (int): The input dimension
-    - end (int): The end value
-    - theta (float): The theta value
-
-    Returns:
-    - torch.Tensor: The precomputed frequencies of shape (end, dim)
-    """
-    freqs = 1.0 / (theta ** (torch.arange(0, dim, 2)[: (dim // 2)].float() / dim))
-    t = torch.arange(end, device=freqs.device, dtype=torch.float32)
-    freqs = torch.outer(t, freqs)
-    freqs_cis = torch.polar(torch.ones_like(freqs), freqs)  # complex64
-    return freqs_cis
-
-
-def reshape_for_broadcast(freqs_cis: torch.Tensor, x: torch.Tensor):
-    """
-    Reshape the frequencies for broadcasting.
-
-    Args:
-    - freqs_cis (torch.Tensor): The frequencies
-    - x (torch.Tensor): The input tensor
-
-    Returns:
-    - torch.Tensor: The reshaped frequencies
-    """
-    ndim = x.ndim
-    assert 0 <= 1 < ndim
-    assert freqs_cis.shape == (x.shape[1], x.shape[-1])
-    shape = [d if i == 1 or i == ndim - 1 else 1 for i, d in enumerate(x.shape)]
-    return freqs_cis.view(*shape)
-
-
-def apply_rotary_emb(
-    xq: torch.Tensor,
-    xk: torch.Tensor,
-    freqs_cis: torch.Tensor,
-) -> Tuple[torch.Tensor, torch.Tensor]:
-    """
-    Apply the rotary embeddings to the input tensors.
-
-    Args:
-    - xq (torch.Tensor): The query tensor
-    - xk (torch.Tensor): The key tensor
-    - freqs_cis (torch.Tensor): The frequencies
-
-    Returns:
-    - Tuple[torch.Tensor, torch.Tensor]: The query and key tensors with the rotary embeddings applied
-    """
-    xq_ = torch.view_as_complex(xq.float().reshape(*xq.shape[:-1], -1, 2))
-    xk_ = torch.view_as_complex(xk.float().reshape(*xk.shape[:-1], -1, 2))
-    freqs_cis = reshape_for_broadcast(freqs_cis, xq_)
-    xq_out = torch.view_as_real(xq_ * freqs_cis).flatten(3)
-    xk_out = torch.view_as_real(xk_ * freqs_cis).flatten(3)
-    return xq_out.type_as(xq), xk_out.type_as(xk)
-
-
-class Attention(nn.Module):
-    """
-    Multi-head attention layer.
-
-    Args:
-    - args (ModelArgs): The model arguments
-
-    Attributes:
-    - n_heads (int): The number of heads
-    - head_dim (int): The dimension of each head
-    - wq (nn.Linear): The query weight matrix
-    - wk (nn.Linear): The key weight matrix
-    - wv (nn.Linear): The value weight matrix
-    - wo (nn.Linear): The output weight matrix
-    """
-    def __init__(self, args: ModelArgs):
-        super().__init__()
-        self.n_heads = args.n_heads
-        self.head_dim = args.dim // args.n_heads
-
-        self.wq = nn.Linear(args.dim, args.n_heads * self.head_dim, bias=False)
-        self.wk = nn.Linear(args.dim, args.n_heads * self.head_dim, bias=False)
-        self.wv = nn.Linear(args.dim, args.n_heads * self.head_dim, bias=False)
-        self.wo = nn.Linear(args.n_heads * self.head_dim, args.dim, bias=False)
-
-    def forward(
-        self,
-        x: torch.Tensor,
-        start_pos: int,
-        freqs_cis: torch.Tensor,
-        mask: Optional[torch.Tensor],
-    ):
-        bsz, seqlen, _ = x.shape
-        xq, xk, xv = self.wq(x), self.wk(x), self.wv(x)
-
-        xq = xq.view(bsz, seqlen, self.n_heads, self.head_dim)
-        xk = xk.view(bsz, seqlen, self.n_heads, self.head_dim)
-        xv = xv.view(bsz, seqlen, self.n_heads, self.head_dim)
-
-        xq, xk = apply_rotary_emb(xq, xk, freqs_cis=freqs_cis)
-
-        keys = xk
-        values = xv
-
-        xq = xq.transpose(1, 2)  # (bs, n_heads, seqlen, head_dim)
-        keys = keys.transpose(1, 2)  # (bs, n_heads, seqlen, head_dim)
-        values = values.transpose(1, 2)  # (bs, n_heads, seqlen, head_dim)
-        scores = torch.matmul(xq, keys.transpose(2, 3)) / math.sqrt(self.head_dim)
-        if mask is not None:
-            scores = scores + mask  # (bs, n_heads, seqlen, seqlen)
-        scores = F.softmax(scores.float(), dim=-1).type_as(xq)
-        output = torch.matmul(scores, values)  # (bs, n_heads, seqlen, head_dim)
-        output = output.transpose(1, 2).contiguous().view(bsz, seqlen, -1)
-        return self.wo(output)
-
-
-class FeedForward(nn.Module):
-    """
-    Feed-forward layer with Swish-Gated Linear Unit (SwiGLU) activation.
-
-    Args:
-    - dim (int): The input dimension
-    - hidden_dim (int): The hidden dimension
-    - multiple_of (int): The multiple of the hidden dimension
-
-    Attributes:
-    - w1 (nn.Linear): The first weight matrix
-    - w2 (nn.Linear): The second weight matrix
-    - w3 (nn.Linear): The third weight matrix
-    """
-    def __init__(
-        self,
-        dim: int,
-        hidden_dim: int,
-        multiple_of: int,
-    ):
-        super().__init__()
-        hidden_dim = int(2 * hidden_dim / 3)
-        hidden_dim = multiple_of * ((hidden_dim + multiple_of - 1) // multiple_of)
-
-        self.w1 = nn.Linear(dim, hidden_dim, bias=False)
-        self.w2 = nn.Linear(hidden_dim, dim, bias=False)
-        self.w3 = nn.Linear(dim, hidden_dim, bias=False)
-
-    def forward(self, x):
-        return self.w2(F.silu(self.w1(x)) * self.w3(x))
-
-
-class TransformerBlock(nn.Module):
-    """
-    Transformer block with attention and feed-forward layers.
-
-    Args:
-    - layer_id (int): The layer ID
-    - args (ModelArgs): The model arguments
-
-    Attributes:
-    - n_heads (int): The number of heads
-    - dim (int): The input dimension
-    - head_dim (int): The dimension of each head
-    - attention (Attention): The attention layer
-    - feed_forward (FeedForward): The feed-forward layer
-    - layer_id (int): The layer ID
-    """
-    def __init__(self, layer_id: int, args: ModelArgs):
-        super().__init__()
-        self.n_heads = args.n_heads
-        self.dim = args.dim
-        self.head_dim = args.dim // args.n_heads
-        self.attention = Attention(args)
-        self.feed_forward = FeedForward(
-            dim=args.dim,
-            hidden_dim=4 * args.dim,
-            multiple_of=args.multiple_of,
-        )
-        self.layer_id = layer_id
-        self.attention_norm = RMSNorm(args.dim, eps=args.norm_eps)
-        self.ffn_norm = RMSNorm(args.dim, eps=args.norm_eps)
-
-    def forward(
-        self,
-        x: torch.Tensor,
-        start_pos: int,
-        freqs_cis: torch.Tensor,
-        mask: Optional[torch.Tensor],
-    ):
-        h = x + self.attention(self.attention_norm(x), start_pos, freqs_cis, mask)
-        out = h + self.feed_forward(self.ffn_norm(h))
-        return out
-
-
-class Transformer(nn.Module):
+class InnerTransformer(nn.Module):
     """
     Transformer model with multiple model blocks.
 
@@ -288,63 +73,27 @@ class Transformer(nn.Module):
     """
     def __init__(self, params: ModelArgs):
         super().__init__()
-        self.params = params
-        self.n_layers = params.n_layers
         # Added : embedding
         self.embedding = torch.nn.Embedding(params.vocab_size, params.dim)
         # -----------------
 
-        self.layers = torch.nn.ModuleList()
-        for layer_id in range(params.n_layers):
-            self.layers.append(TransformerBlock(layer_id, params))
-
-        self.norm = RMSNorm(params.dim, eps=params.norm_eps)
-
-        self.freqs_cis = precompute_freqs_cis(
-            params.dim // params.n_heads,
-            params.max_seq_len * 2,
-            params.rope_theta,
-        )
+        self.transformer = model.Transformer(params)
 
         # Added : final projection (into distribution over vocabulary)
         self.final_proj = torch.nn.Linear(params.dim, params.vocab_size)
         # -----------------
 
-
     def forward(self, input_tensor: torch.Tensor, start_pos: int = 0):
         # Added : embedding
         input_tensor = self.embedding(input_tensor) # (B, S) -> (B, S, dim)
         # -----------------
-
-        _bsz, seqlen, _ = input_tensor.shape
-        h = input_tensor
-        self.freqs_cis = self.freqs_cis.to(h.device)
-        freqs_cis = self.freqs_cis[start_pos: start_pos + seqlen]
-
-        mask = None
-        if seqlen > 1:
-            mask = torch.full((seqlen, seqlen), float("-inf"), device=input_tensor.device)
-
-            mask = torch.triu(mask, diagonal=1)
-
-            # When performing key-value caching, we compute the attention scores
-            # only for the new sequence. Thus, the matrix of scores is of size
-            # (seqlen, cache_len + seqlen), and the only masked entries are (i, j) for
-            # j > cache_len + i, since row i corresponds to token cache_len + i.
-            mask = torch.hstack(
-                [torch.zeros((seqlen, start_pos), device=input_tensor.device), mask]
-            ).type_as(h)
-
-        for layer in self.layers:
-            h = layer(h, start_pos, freqs_cis, mask)
-        h = self.norm(h)
-
+        
+        h = self.transformer(input_tensor)
+        
         # Added : final projection
         h = self.final_proj(h)
         # -----------------
-
-        output = h.float()
-        return output
+        return h
 
 # II. Adjusted trainer
 
@@ -375,7 +124,7 @@ def setup_clearml():
     )
 
 
-class Trainer:
+class TransformerTrainer:
     def __init__(self, lr: float = 2e-4, weight_decay: float = 3e-5,
                  batch_norm_momentum: float | None = 0.002, n_epochs: int = 10,
                  device: str = DEVICE, extra_augmentation: v2.Transform | None = None):
@@ -614,8 +363,8 @@ def test_hyperparameters(dim, n_layers, n_heads, rope_theta, n_epochs, batch_siz
     # Set up the model args from ModelArgs dataclass
     args = ModelArgs(dim=dim, n_layers=n_layers, n_heads=n_heads, rope_theta=rope_theta)
     # Intialize the trainer and model
-    model = Transformer(args).to(DEVICE)
-    trainer = Trainer(n_epochs=n_epochs, lr=lr)
+    model = InnerTransformer(args).to(DEVICE)
+    trainer = TransformerTrainer(n_epochs=n_epochs, lr=lr)
 
     # Create a new ClearML task for each run
     params = {
