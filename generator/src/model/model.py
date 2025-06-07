7bfc35a5422d67a5e461e775f6481ee0f43b6f17
Ajaskula <adamjask@gmail.com>
2025-06-07 23:32:32 +0200

    Delete model pipeline

--- generator/src/model/model.py
+++ generator/src/model/model.py
@@ -542,14 +542,5 @@ class SpatioTemporalTransformer(nn.Module):
         return x
 
 
-if __name__ == "__main__":
-    args = ModelArgs()
-    model = SpatioTemporalTransformer(args)
-    frames = transformations.transform_gif_to_tensor()
-    frames = transformations.transform_image_to_trainable_form(frames)
-    print(frames.shape)
-    output = model(frames)
-    print(output.shape)
-
 
 
